7f3da748c912532f6bec966a94b96830ce7e2a42
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-16 21:55:54 +0530

    [Chandra]

---
+++ WeekDays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Constructs
+{
+    public class WeekDays
+    {
+        public static void Day()
+        {
+            Console.WriteLine("Enter the Number");
+            int day=Convert.ToInt32(Console.ReadLine());
+            switch(day) 
+            {
+                case 1: Console.WriteLine("Sunday"); break;
+                case 2: Console.WriteLine("Monday");break;
+                case 3: Console.WriteLine("Tuesday");break;
+                case 4: Console.WriteLine("Wednesday");break;
+                case 5: Console.WriteLine("Thrusday");break;
+                case 6: Console.WriteLine("Friday");break;
+                case 7: Console.WriteLine("Saturday");break;
+
+                   default: Console.WriteLine("Enter correct number");break;
+            }
+        }
+    }
+}
