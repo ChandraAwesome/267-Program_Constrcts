a19a501f4d60167646ff2c0141a84087c284a415
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-16 21:59:02 +0530

    [Chandra]

---
+++ FlipCoin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Constructs
+{
+    internal class FlipCoin
+    {
+        public static void HeadorTail()
+        {
+            int head = 0;
+            int tail = 0;
+            Random toss = new Random();
+            while (head < 20 || tail < 20)
+            {
+                int result = toss.Next(0, 2);
+                if (result == 0)
+                {
+                    Console.WriteLine("It is Head");
+                    head++;
+                }
+                else
+                {
+                    Console.WriteLine("It is Tail");
+                    tail++;
+                }
+            }
+            if (head == 20)
+            {
+                Console.WriteLine("Head wins 20 times");
+            }
+            else
+            {
+                Console.WriteLine("Tail wins 20 times");
+            }
+        }
+    }
+}
