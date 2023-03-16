35e00212caaf31d2b3fb2074f48ccb76d3f4e635
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-16 21:59:25 +0530

    [Chandra]

---
+++ Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Constructs
+{
+    internal class Factorial
+    {
+        public static void fact()
+        {
+            Console.WriteLine("Enter the Number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int m = 1;
+            for(int i=1;i<=N;i++) 
+            {
+                m = m * i;
+            }
+            Console.WriteLine("Factorial of " + N + " is " + m);
+        }
+    }
+}
